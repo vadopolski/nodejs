f5849a32c2fb0d8e17b57b17ef129b0ae831a9c3
vadopolski <vop444@gmail.com>
2018-04-15 15:40:21 +0300

    Added server validation.

--- hello.js
+++ hello.js
@@ -4,13 +4,30 @@
 
 var http = require('http');
 var url = require('url');
-var count = 0;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     var q = url.parse(req.url, true).query;
+    var header = "<form action=\"http://localhost:8080/\">";
+    var inputFirstName = "</legend>First name:<br>" +
+        "<input type=\"text\" name=\"firstname\" value=\"Mickey\">";
+    var inputLastName = "<br><br></legend>Last name:<br>" +
+        "<input type=\"text\" name=\"lastname\" value=\"Mouse\">";
+    var footer = "<br><br><input type=\"submit\" value=\"Submit\">" +
+        "</form>";
+    var wrongMessage = "<span style=\"color: red\"> Wrong </span><br>";
 
-    res.end('Hello ' + q.firstname + ' ' + q.lastname + '!!!!!');
+    var html = header + inputFirstName + inputLastName + footer;
 
-    console.log("Number is" + ++count);
+    if(q.firstname == "test"){
+        var q = url.parse(req.url, true).query;
+        res.write('Hello ' + q.firstname + ' ' + q.lastname + '!!!!!');
+    } else {
+
+        var html = header + inputFirstName + wrongMessage + inputLastName + footer;
+
+        res.write(html);
+    }
+
+    res.end();
 }).listen(8080);
